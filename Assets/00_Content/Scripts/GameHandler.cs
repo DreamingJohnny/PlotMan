f61c20f846e137944358cffbcfc053c03d7de436
DreamingJohnny <johnnydreaming@gmail.com>
2023-12-15 13:31:41 +0100

    Added grid, tilemap, cinemachine and resized walls

--- Assets/00_Content/Scripts/GameHandler.cs
+++ Assets/00_Content/Scripts/GameHandler.cs
@@ -6,46 +6,50 @@ public class GameHandler : MonoBehaviour {
 
 	[SerializeField] private PlayerController player;
 
-	//[SerializeField] private LevelHandler levelHandler;
+	[SerializeField] private LevelHandler levelHandler;
 
-	[Header("Grid Settings")]
-	[SerializeField] private int gridWidth;
-	[SerializeField] private int gridHeight;
-	[SerializeField] private float cellSize;
-	[SerializeField] private Vector2 startingOffset;
+	//[Header("Grid Settings")]
+	//[SerializeField] private int gridWidth;
+	//[SerializeField] private int gridHeight;
+	//[SerializeField] private float cellSize;
+	//[SerializeField] private Vector2 startingOffset;
 
-	private Grid grid;
+	//private Grid grid;
 
 	[Header("Pathfinding")]
 	//TODO: Later on, it's possible we'll want the pathfinder to handle the grid, and hold that, rather than, as now, having the gameHandler hold it.
 	//Although, I suppse it would make more sense if both pathfinding and the grid, and the grid setup, lay on a separate "Level" object. Then, gameHandler could just communicate with it.
-	private Pathfinding pathfinder;
 
-	[SerializeField] private PathTester pathTester;
+	//private Pathfinding pathfinder;
+
+	//[SerializeField] private PathTester pathTester;
 	//This is also completely for testing, and should soon be removed.
 	[SerializeField] private EnemyGhost enemyGhost;
 
 	[SerializeField] private List<Vector2> testRoute;
 
 	void Start() {
-		grid = new Grid(gridWidth, gridHeight, cellSize, startingOffset);
+		//grid = new Grid(gridWidth, gridHeight, cellSize, startingOffset);
 
-		pathfinder = new Pathfinding(grid);
+		//pathfinder = new Pathfinding(grid);
 
-		player.SetPosition(GetCellMidPoint(0, 0));
+		//Actually, later on, gameHandler should be sending in the SO_LevelData here, to tell the level to set it all up.
+		levelHandler.SetUpLevel();
 
-		pathTester.TestPath(pathfinder, grid);
+		player.SetPosition(levelHandler.GetPlayerSpawnPoint());
 
-		enemyGhost.TeleportToCell(grid.GetCell(10, 10));
+		//pathTester.TestPath(pathfinder, grid);
 
-		enemyGhost.SetRoute(pathfinder.GetPath(enemyGhost.CurrentCell, grid.GetCell(12, 8)));
+		//enemyGhost.TeleportToCell(grid.GetCell(10, 10));
+
+		//enemyGhost.SetRoute(pathfinder.GetPath(enemyGhost.CurrentCell, grid.GetCell(12, 8)));
 	}
 
 	void Update() {
 
 	}
 
-	public Vector2 GetCellMidPoint(int x, int y) {
-		return grid.GetCellMidPoint(x, y);
-	}
+	//public Vector2 GetCellMidPoint(int x, int y) {
+	//	return grid.GetCellMidPoint(x, y);
+	//}
 }
--- Assets/00_Content/Scripts/Level/LevelHandler.cs
+++ Assets/00_Content/Scripts/Level/LevelHandler.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,9 +11,22 @@ public class LevelHandler : MonoBehaviour {
 	[SerializeField] private float cellSize;
 	[SerializeField] private Vector2 startingOffset;
 
+	//Here I'll make a series of values for things like,
+	//where enemies should spawn,
+	//where the player should spawn,
+	//and where the powers should spawn.
+	//Later on, it seems logical that these will also become part of the "levelData".
+	//TODO: Also, these should only be able to be ints, never floats, might be worth looking into how to set that up as well.
+	[SerializeField] private Vector2 playerSpawnIndex;
+	[SerializeField] private List<Vector2> enemySpawnIndexes;
+	[SerializeField] private List<Vector2> powerUpSpawnIndexes;
+
 	//Later on, this will contain all of the information needed for setting up the maze, including the values currently stored under "Grid Settings".
 	//On top of that, this will later also hold the sprites for the maze.
 	[SerializeField] SO_LevelData levelData;
+
+	[SerializeField] private PowerPoint powerPoint;
+	[SerializeField] private EnemyGhost enemyGhost;
 
 	private Grid grid;
 
@@ -20,13 +34,44 @@ public class LevelHandler : MonoBehaviour {
 	[Header("Pathfinding")]
 	private Pathfinding pathfinder;
 
-
-
 	void Start() {
-
+		
 	}
 
 	void Update() {
 
+	}
+
+	public void SetUpLevel() {
+		grid = new Grid(gridWidth, gridHeight, cellSize, startingOffset);
+
+		pathfinder = new Pathfinding(grid);
+
+		SpawnPowerPoints();
+
+		SpawnEnemies();
+	}
+
+	private void SpawnEnemies() {
+		if (enemyGhost == null) return;
+
+		foreach (Vector2 vector2 in enemySpawnIndexes) {
+			Instantiate(enemyGhost, grid.GetCellMidPoint(Mathf.CeilToInt(vector2.x), Mathf.CeilToInt(vector2.y)), UnityEngine.Quaternion.identity);
+		}
+	}
+
+	private void SpawnPowerPoints() {
+		if (powerPoint == null) return;
+
+		foreach (Vector2 vector2 in powerUpSpawnIndexes) {
+			Instantiate(powerPoint, grid.GetCellMidPoint(Mathf.CeilToInt(vector2.x), Mathf.CeilToInt(vector2.y)), UnityEngine.Quaternion.identity);
+		}
+	}
+
+	/// <summary>
+	/// Returns the real-world coordinates for where the player should spawn.
+	/// </summary>	
+	public Vector2 GetPlayerSpawnPoint() {
+		return grid.GetCellMidPoint(Mathf.CeilToInt(playerSpawnIndex.x), Mathf.CeilToInt(playerSpawnIndex.y));
 	}
 }
